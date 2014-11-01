c62135308dd4998fc6fdc8b3bb4c9360e06ab8f2
Ryp <ryp.sqrt@gmail.com>
2014-11-01 15:02:19 +0000

    Cpp0x refactor for controller

--- src/AController.cpp
+++ src/AController.cpp
@@ -1,7 +1,7 @@
 #include "AController.hpp"
 
 AController::AController(int buttons, int axes)
-  : _buttons(buttons),
+:   _buttons(buttons),
     _axes(axes)
 {}
 
@@ -9,44 +9,44 @@ AController::~AController() {}
 
 void AController::reset()
 {
-  Button b;
+    Button b;
 
-  b.held = false;
-  b.new_held = false;
-  b.pressed = false;
-  b.released = false;
-  for (std::vector<Button>::iterator it = _buttons.begin(); it != _buttons.end(); it++)
-    *it = b;
-  for (std::vector<Axe>::iterator it = _axes.begin(); it != _axes.end(); it++)
-    (*it) = 0;
+    b.held = false;
+    b.new_held = false;
+    b.pressed = false;
+    b.released = false;
+    for (auto& button : _buttons)
+        button = b;
+    for (auto& axe : _axes)
+        axe = 0;
 }
 
 void AController::update()
 {
-  for (std::vector<Button>::iterator it = _buttons.begin(); it != _buttons.end(); it++)
-  {
-    (*it).pressed = !(*it).held && (*it).new_held;
-    (*it).released = (*it).held && !(*it).new_held;
-    (*it).held = (*it).new_held;
-  }
+    for (auto& button : _buttons)
+    {
+        button.pressed = !button.held && button.new_held;
+        button.released = button.held && !button.new_held;
+        button.held = button.new_held;
+    }
 }
 
 bool AController::isHeld(unsigned int idx) const
 {
-  return (_buttons[idx].held);
+    return (_buttons[idx].held);
 }
 
 bool AController::isPressed(unsigned int idx) const
 {
-  return (_buttons[idx].pressed);
+    return (_buttons[idx].pressed);
 }
 
 bool AController::isReleased(unsigned int idx) const
 {
-  return (_buttons[idx].released);
+    return (_buttons[idx].released);
 }
 
 float AController::getAxis(unsigned int idx) const
 {
-  return (_axes[idx]);
+    return (_axes[idx]);
 }
