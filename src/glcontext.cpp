fffbdfd272030dd9ff5da26bf832cad08e9a81ff
Ryp <ryp.sqrt@gmail.com>
2014-11-03 12:44:07 +0000

    Discovered bug in 4.4 core profile

--- src/glcontext.cpp
+++ src/glcontext.cpp
@@ -23,6 +23,11 @@ void GLContext::create(unsigned int width, unsigned int height, bool fullscreen)
 {
     if (!glfwInit())
         throw (std::runtime_error("Failed to initialize GLFW"));
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+//     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+//     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
+//     glfwWindowHint(GLFW_RESIZABLE, 0);
     GLFWmonitor* monitor;
     if (!(monitor = glfwGetPrimaryMonitor()))
         throw (std::runtime_error("Failed to retrieve primary monitor"));
