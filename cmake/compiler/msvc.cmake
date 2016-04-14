96f61ecdb42a2497999608edcd633e882250f0f3
Ryp <ryp.sqrt@gmail.com>
2016-04-14 11:40:11 +0200

    Update build system

---
+++ cmake/compiler/msvc.cmake
@@ -0,0 +1,13 @@
+if(MSVC)
+    if(MSVC_VERSION LESS 1700)
+        message(FATAL_ERROR "This version of Visual Studio is not supported")
+    endif()
+
+    set(REAPERGL_DEPS_LOCATION $ENV{REAPERGL_DEPS_LOCATION} CACHE PATH "Path to dependencies folder")
+    if (REAPERGL_DEPS_LOCATION STREQUAL "")
+        message(FATAL_ERROR "Please add the location of the dependencies folder")
+    endif()
+
+    #set(BOOST_ROOT ${REAPERGL_DEPS_LOCATION}/boost CACHE PATH "RakNet location")
+    #add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+endif()
