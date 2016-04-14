4ed18284b1610985dba25f6ddedc264d45ab7a38
Ryp <ryp.sqrt@gmail.com>
2016-04-14 10:45:17 +0200

    Add custom allocators and memory utils

---
+++ src/core/memory/StackAllocator.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+/// ReaperGL
+///
+/// Copyright (c) 2016 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#include "StackAllocator.h"
+
+#include <stdexcept>
+
+StackAllocator::StackAllocator(std::size_t sizeBytes)
+:   _memPtr(new char[sizeBytes]),
+    _memSize(sizeBytes),
+    _currentMarker(0)
+{}
+
+StackAllocator::~StackAllocator()
+{
+    delete[] _memPtr;
+}
+
+void* StackAllocator::alloc(std::size_t sizeBytes)
+{
+    void* ptr = _memPtr + _currentMarker;
+
+    _currentMarker += sizeBytes;
+    Assert(_currentMarker <= _memSize, "Out of memory!");
+    return ptr;
+}
+
+void StackAllocator::freeToMarker(std::size_t marker)
+{
+    _currentMarker = marker;
+}
+
+std::size_t StackAllocator::getMarker() const
+{
+    return _currentMarker;
+}
+
+void StackAllocator::clear()
+{
+    _currentMarker = 0;
+}
