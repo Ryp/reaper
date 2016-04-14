fdaf9d1ae20d81780442e7a219d068bf890591b8
Ryp <ryp.sqrt@gmail.com>
2016-04-14 11:05:13 +0200

    Fix Assert() details

--- src/core/Platform.h
+++ src/core/Platform.h
@@ -1,5 +1,12 @@
-#ifndef PLATFORM_INCLUDED
-#define PLATFORM_INCLUDED
+////////////////////////////////////////////////////////////////////////////////
+/// ReaperGL
+///
+/// Copyright (c) 2016 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef REAPER_PLATFORM_INCLUDED
+#define REAPER_PLATFORM_INCLUDED
 
 // Identify the operating system.
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__MINGW32__)
@@ -29,11 +36,25 @@
     #define REAPERGL_EXPORT __declspec(dllexport)
     #define REAPERGL_IMPORT __declspec(dllimport)
 #elif defined(__GNUC__)
-    #define REAPERGL_EXPORT __attribute__((visibility("default")))
+    #define REAPERGL_EXPORT __attribute__((visibility("hidden")))
     #define REAPERGL_IMPORT
 #else
     #define REAPERGL_EXPORT
     #define REAPERGL_IMPORT
 #endif
 
-#endif // PLATFORM_INCLUDED
+// NDEBUG must be defined (or not) by the build system
+#if !defined(NDEBUG)
+    #define DEBUG
+#endif
+
+constexpr bool isDebugBuild()
+{
+#if defined(DEBUG)
+    return true;
+#else
+    return false;
+#endif
+}
+
+#endif // REAPER_PLATFORM_INCLUDED
