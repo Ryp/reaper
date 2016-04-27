af04d8b9870b8e001836c77a1e6511a6823d81a1
Ryp <ryp.sqrt@gmail.com>
2016-04-27 23:40:37 +0200

    Add compiler detection

---
+++ src/core/Compiler.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+/// ReaperGL
+///
+/// Copyright (c) 2015-2016 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef REAPER_COMPILER_INCLUDED
+#define REAPER_COMPILER_INCLUDED
+
+#if defined(__INTEL_COMPILER)
+    #define REAPER_COMPILER_ICC
+#elif defined(__GNUC__)
+    #define REAPER_COMPILER_GCC
+#elif defined(__clang__)
+    #define REAPER_COMPILER_CLANG
+#elif defined(_MSC_VER)
+    #define REAPER_COMPILER_MSVC
+#else
+    #error Compiler not recognised!
+#endif
+
+#endif // REAPER_COMPILER_INCLUDED
