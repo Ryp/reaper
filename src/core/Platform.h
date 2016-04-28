b426e1f53cd8866c02751d172304407d81e4801f
Ryp <ryp.sqrt@gmail.com>
2016-04-28 23:30:12 +0200

    Minor macro tweaks

--- src/core/Platform.h
+++ src/core/Platform.h
@@ -9,14 +9,14 @@
 #define REAPER_PLATFORM_INCLUDED
 
 // Identify the operating system.
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__MINGW32__)
+#if defined(WIN32) || defined(_WIN32) || defined(__MINGW32__)
     #define REAPERGL_PLATFORM_WINDOWS
     #define WIN32_LEAN_AND_MEAN
     #define NOMINMAX
     #include <windows.h>
 #elif defined(linux) || defined(__linux) || defined(__linux__)
     #define REAPERGL_PLATFORM_LINUX
-#elif defined(__APPLE__) || defined(__MACH__)
+#elif defined(__MACOSX__) || (defined(__APPLE__) && defined(__MACH__)
     #define REAPERGL_PLATFORM_MACOSX
 #else
     #error Platform not recognised!
