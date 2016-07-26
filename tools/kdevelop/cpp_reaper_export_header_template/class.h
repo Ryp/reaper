197138b772906a7dac4a6ec0191550a7747efec1
Ryp <ryp.sqrt@gmail.com>
2016-07-26 21:48:47 +0200

    Update export header template

--- tools/kdevelop/cpp_reaper_export_header_template/class.h
+++ tools/kdevelop/cpp_reaper_export_header_template/class.h
@@ -16,12 +16,17 @@
 
 
 // Make sure this is up to date with the build system.
-#if defined(reaper_{{ name|lower }}_EXPORTS)
-#define REAPER_{{ name|upper }}_API REAPER_EXPORT
+#if defined(REAPER_BUILD_SHARED)
+    #if defined(reaper_{{ name|lower }}_EXPORTS)
+        #define REAPER_{{ name|upper }}_API REAPER_EXPORT
+    #else
+        #define REAPER_{{ name|upper }}_API REAPER_IMPORT
+    #endif
+#elif defined(REAPER_BUILD_STATIC)
+    #define REAPER_{{ name|upper }}_API
 #else
-#define REAPER_{{ name|upper }}_API REAPER_IMPORT
+    #error Build type must be defined
 #endif
-
 
 {% block include_guard_close %}
 #endif // REAPER_{{ name|upper }}_EXPORT_INCLUDED
