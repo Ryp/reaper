9c4faf8a235a21ed16c051d7fab539044d7d302b
Ryp <ryp.sqrt@gmail.com>
2017-03-15 00:22:27 +0100

    Improve microprofile support

---
+++ src/core/Profile.h
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Reaper
+///
+/// Copyright (c) 2015-2017 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef REAPER_PROFILE_INCLUDED
+#define REAPER_PROFILE_INCLUDED
+
+#if defined(REAPER_USE_MICROPROFILE)
+    #include "microprofile.h"
+    #define REAPER_PROFILE_SCOPE(group, color)          MICROPROFILE_SCOPEI(group, __func__, color)
+#else
+    #define REAPER_PROFILE_SCOPE(group, name, color) do{}while(0)
+#endif
+
+#endif // REAPER_PROFILE_INCLUDED
+
