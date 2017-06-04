a131aab420161b600db00ed327d0a614b8569019
Ryp <ryp.sqrt@gmail.com>
2017-06-04 14:53:50 +0200

    blender: added auto export to obj example. Can be used to export data from .blender projects in CLI

---
+++ tools/blender/autoexport.py
@@ -0,0 +1,22 @@
+import bpy
+
+# access the scene
+scene = bpy.context.scene
+
+# start with empty selection
+bpy.ops.object.select_all(action='DESELECT')
+
+# loop through all objects in the scene
+for ob in scene.objects:
+
+    # select this object
+    scene.objects.active = ob
+    ob.select = True
+
+    # if this object is a mesh
+    if ob.type == 'MESH':
+
+        bpy.ops.export_scene.obj(filepath=ob.name + '.obj', use_selection=True)
+
+    # deselect this object again
+    ob.select = False
