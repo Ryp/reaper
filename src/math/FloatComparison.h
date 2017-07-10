89a0799f6c974c958a2ccba732cd4ea1df9ff13a
Ryp <ryp.sqrt@gmail.com>
2017-07-10 23:00:52 +0200

    math: added IsEqualWithEpsilon

---
+++ src/math/FloatComparison.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Reaper
+///
+/// Copyright (c) 2015-2017 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <cmath>
+
+namespace Reaper::Math
+{
+    constexpr float DefaultEpsilon = 1e-4f;
+
+    inline bool IsEqualWithEpsilon(float lhs, float rhs, float epsilon = DefaultEpsilon)
+    {
+        return std::abs(rhs - lhs) < epsilon;
+    }
+}
