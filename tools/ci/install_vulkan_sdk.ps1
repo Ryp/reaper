86bca99ea8b46f19838f7537fc10910adb7e24c1
Ryp <ryp.sqrt@gmail.com>
2018-06-13 00:35:14 +0200

    cmake: fixed vulkan path hints

--- tools/ci/install_vulkan_sdk.ps1
+++ tools/ci/install_vulkan_sdk.ps1
@@ -1,6 +1,7 @@
 #Requires -RunAsAdministrator
 
-$vulkan_sdk_installer = 'VulkanSDK-1.1.70.1-Installer.exe'
+$vulkan_sdk_version = "1.1.70.1"
+$vulkan_sdk_installer = "VulkanSDK-${vulkan_sdk_version}-Installer.exe"
 $vulkan_sdk_url = "https://www.dropbox.com/s/b9xyr1q43p2ov7z/${vulkan_sdk_installer}?dl=1"
 $vulkan_sdk_md5 = 'd05fc5b8ba6f2bdd85e6ac0b2dbbe181'
 
@@ -18,3 +19,6 @@ if ($download_md5.Hash -ne $vulkan_sdk_md5)
 # Execute installer in silent mode and wait for completion
 Write-Host "Installing Vulkan SDK..."
 Invoke-Expression "& .\$vulkan_sdk_installer /S | Out-Null"
+
+# Set VK_SDK_PATH env var for current shell
+$env:VK_SDK_PATH = "c:\VulkanSDK\$vulkan_sdk_version"
