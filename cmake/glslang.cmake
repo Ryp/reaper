2eacdae9801e4bd84410cfe384e52bc3ae79510b
Ryp <ryp.sqrt@gmail.com>
2018-06-13 00:35:14 +0200

    cmake: fixed vulkan path hints

--- cmake/glslang.cmake
+++ cmake/glslang.cmake
@@ -5,11 +5,13 @@
 #// This file is distributed under the MIT License
 #///////////////////////////////////////////////////////////////////////////////
 
-find_program(VULKAN_GLSLANGVALIDATOR_EXEC glslangValidator
-    HINT "$ENV{VULKAN_SDK}/Bin")
+find_program(VULKAN_GLSLANGVALIDATOR_EXEC glslangValidator HINTS
+    "$ENV{VULKAN_SDK}/Bin"
+    "$ENV{VK_SDK_PATH}/Bin")
 
-find_program(VULKAN_SPIRV_VAL_EXEC spirv-val
-    HINT "$ENV{VULKAN_SDK}/Bin")
+find_program(VULKAN_SPIRV_VAL_EXEC spirv-val HINTS
+    "$ENV{VULKAN_SDK}/Bin"
+    "$ENV{VK_SDK_PATH}/Bin")
 
 if(NOT VULKAN_GLSLANGVALIDATOR_EXEC)
     message(FATAL_ERROR "${VULKAN_GLSLANGVALIDATOR_EXEC} not found")
