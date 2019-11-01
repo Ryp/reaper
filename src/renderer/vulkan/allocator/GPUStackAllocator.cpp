9d98f4fbc9c38dd06d4fb31ef59c23a166510f8c
Ryp <ryp.sqrt@gmail.com>
2019-11-01 23:58:35 +0300

    vulkan: improve GPU allocator

--- src/renderer/vulkan/allocator/GPUStackAllocator.cpp
+++ src/renderer/vulkan/allocator/GPUStackAllocator.cpp
@@ -7,23 +7,39 @@
 
 #include "GPUStackAllocator.h"
 
-GPUStackAllocator::GPUStackAllocator(std::size_t size)
-    : _size(size)
-    , _offset(0)
-{}
-
-GPUStackAllocator::~GPUStackAllocator()
-{}
-
-std::size_t GPUStackAllocator::alloc(std::size_t size, std::size_t alignment)
+namespace Reaper
 {
-    std::size_t allocOffset = alignOffset(_offset, alignment);
+VirtualStackAllocator::VirtualStackAllocator(std::size_t size)
+    : m_size(size)
+    , m_offset(0)
+{}
 
-    _offset = allocOffset + size;
-
+std::size_t VirtualStackAllocator::alloc(std::size_t size, std::size_t alignment)
+{
     Assert(size > 0, "Invalid alloc size");
     Assert(alignment > 0, "Invalid alignment size");
-    Assert(_offset <= _size, "Out of memory!");
+
+    const std::size_t allocOffset = alignOffset(m_offset, alignment);
+
+    m_offset = allocOffset + size;
+
+    Assert(m_offset <= m_size, "Out of memory!");
 
     return allocOffset;
 }
+
+GPUStackAllocator::GPUStackAllocator(VkDeviceMemory memory, u32 index, std::size_t size)
+    : m_allocator(size)
+    , m_memory(memory)
+    , m_memoryIndex(index)
+{}
+
+GPUAlloc GPUStackAllocator::alloc(VkMemoryRequirements requirements)
+{
+    Assert(m_memoryIndex & requirements.memoryTypeBits, "Incompatible memory type");
+
+    const std::size_t offset = m_allocator.alloc(requirements.size, requirements.alignment);
+
+    return GPUAlloc{m_memory, offset, requirements.size};
+}
+} // namespace Reaper
