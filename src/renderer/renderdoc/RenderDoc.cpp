51ea9f26d8102d18a8597cc67be8ae7830a1b01f
Ryp <thibault.schueller@protonmail.com>
2019-11-05 00:05:41 +0300

    renderdoc: improve cmake integration
    Provide FindRenderDoc.cmake to allow automatic locating.
    Fix windows include search.

--- src/renderer/renderdoc/RenderDoc.cpp
+++ src/renderer/renderdoc/RenderDoc.cpp
@@ -7,21 +7,19 @@
 
 #include "RenderDoc.h"
 
-#include "common/DebugLog.h"
-#include "common/ReaperRoot.h"
-
 #if defined(REAPER_USE_RENDERDOC)
+
+#    include "common/DebugLog.h"
+#    include "common/ReaperRoot.h"
 
 #    include "core/DynamicLibrary.h"
 #    include "core/Profile.h"
 
 #    if defined(REAPER_PLATFORM_LINUX) || defined(REAPER_PLATFORM_MACOSX)
-#        define RENDERDOC_LIB_NAME "librenderdoc.so"
+#        include <renderdoc.h>
 #    elif defined(REAPER_PLATFORM_WINDOWS)
-#        define RENDERDOC_LIB_NAME "renderdoc.dll"
+#        include <renderdoc_app.h>
 #    endif
-
-#    include <renderdoc.h>
 
 namespace Reaper::RenderDoc
 {
@@ -42,9 +40,9 @@ void start_integration(ReaperRoot& root)
 
     Assert(g_renderDocLib == nullptr);
 
-    log_info(root, "renderdoc: loading {}", RENDERDOC_LIB_NAME);
+    log_info(root, "renderdoc: loading {}", REAPER_RENDERDOC_LIB_NAME);
     // FIXME Documentation recommends RTLD_NOW | RTLD_NOLOAD on linux
-    g_renderDocLib = dynlib::load(RENDERDOC_LIB_NAME);
+    g_renderDocLib = dynlib::load(REAPER_RENDERDOC_LIB_NAME);
 
     pRENDERDOC_GetAPI pfn_RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dynlib::getSymbol(g_renderDocLib, "RENDERDOC_GetAPI");
 
@@ -66,8 +64,9 @@ void stop_integration(ReaperRoot& root)
 
     Assert(g_renderDocLib != nullptr);
 
-    // FIXME https://github.com/bkaradzic/bgfx/issues/1192
-    // log_info(root, "renderdoc: unloading {}", RENDERDOC_LIB_NAME);
+    // We are not supposed to unload RenderDoc lib manually ever.
+    // See also: https://github.com/bkaradzic/bgfx/issues/1192
+    // log_info(root, "renderdoc: unloading {}", REAPER_RENDERDOC_LIB_NAME);
     // dynlib::close(g_renderDocLib);
     // g_renderDocLib = nullptr;
 }
@@ -77,14 +76,14 @@ void stop_integration(ReaperRoot& root)
 
 namespace Reaper::RenderDoc
 {
-void start_integration(ReaperRoot& root)
+void start_integration(ReaperRoot& /*root*/)
 {
-    log_info(root, "renderdoc: support disabled");
+    AssertUnreachable();
 }
 
-void stop_integration(ReaperRoot& root)
+void stop_integration(ReaperRoot& /*root*/)
 {
-    log_info(root, "renderdoc: support disabled");
+    AssertUnreachable();
 }
 } // namespace Reaper::RenderDoc
 
