c4f8405ba7edc48e7a02d44070eced48e37c4f59
Ryp <thibault.schueller@protonmail.com>
2019-12-05 19:55:54 +0300

    vulkan: add HLSL shader support through glslangValidator
    Ported the currently used shader to HLSL for testing.
    Add spirv-opt as an optimizing pass but also to legalize HLSL code.

--- cmake/glslang.cmake
+++ cmake/glslang.cmake
@@ -9,12 +9,20 @@ find_program(VULKAN_GLSLANGVALIDATOR_EXEC glslangValidator HINTS
     "$ENV{VULKAN_SDK}/Bin"
     "$ENV{VK_SDK_PATH}/Bin")
 
+find_program(VULKAN_SPIRV_OPT_EXEC spirv-opt HINTS
+    "$ENV{VULKAN_SDK}/Bin"
+    "$ENV{VK_SDK_PATH}/Bin")
+
 find_program(VULKAN_SPIRV_VAL_EXEC spirv-val HINTS
     "$ENV{VULKAN_SDK}/Bin"
     "$ENV{VK_SDK_PATH}/Bin")
 
 if(NOT VULKAN_GLSLANGVALIDATOR_EXEC)
     message(FATAL_ERROR "${VULKAN_GLSLANGVALIDATOR_EXEC} not found")
+endif()
+
+if(NOT VULKAN_SPIRV_OPT_EXEC)
+    message(FATAL_ERROR "${VULKAN_SPIRV_OPT_EXEC} not found")
 endif()
 
 if(NOT VULKAN_SPIRV_VAL_EXEC)
@@ -24,7 +32,7 @@ endif()
 # This function will create SPIR-V targets for each input glsl file and append it
 # to the user-provided 'generated_files' variable.
 # Then make your desired target depend on them and it will automatically compile your shaders.
-function(add_glslang_spirv_targets shader_root_folder generated_files)
+function(add_glslang_spirv_targets shader_root_folder use_hlsl generated_files)
     if(${ARGC} LESS_EQUAL 2)
         message(FATAL_ERROR "No shaders were passed to the function")
     endif()
@@ -36,14 +44,28 @@ function(add_glslang_spirv_targets shader_root_folder generated_files)
         get_filename_component(OUTPUT_SPIRV_PATH ${OUTPUT_SPIRV} DIRECTORY)
         get_filename_component(OUTPUT_SPIRV_NAME ${OUTPUT_SPIRV} NAME)
 
-        add_custom_command(OUTPUT ${OUTPUT_SPIRV}
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_SPIRV_PATH}
-            COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} -V ${INPUT_GLSL} -o ${OUTPUT_SPIRV}
-            COMMAND ${VULKAN_SPIRV_VAL_EXEC} ${OUTPUT_SPIRV}
-            MAIN_DEPENDENCY ${INPUT_GLSL}
-            COMMENT "Compiling GLSL shader ${INPUT_GLSL_REL} into SPIR-V (${OUTPUT_SPIRV_NAME})"
-            VERBATIM
-        )
+        # Watch GDC 2018 - HLSL in Vulkan
+        # https://www.youtube.com/watch?v=42lqJ-iXc7g
+        if(${use_hlsl})
+            add_custom_command(OUTPUT ${OUTPUT_SPIRV}
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_SPIRV_PATH}
+                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} -e main -V -D ${INPUT_GLSL} -o ${OUTPUT_SPIRV}.unoptimized
+                COMMAND ${VULKAN_SPIRV_OPT_EXEC} ${OUTPUT_SPIRV}.unoptimized --legalize-hlsl -Os -o ${OUTPUT_SPIRV}
+                COMMAND ${VULKAN_SPIRV_VAL_EXEC} ${OUTPUT_SPIRV}
+                MAIN_DEPENDENCY ${INPUT_GLSL}
+                COMMENT "Compiling HLSL shader ${INPUT_GLSL_REL} into SPIR-V (${OUTPUT_SPIRV_NAME})"
+                VERBATIM)
+        else()
+            add_custom_command(OUTPUT ${OUTPUT_SPIRV}
+                COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_SPIRV_PATH}
+                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} -V ${INPUT_GLSL} -o ${OUTPUT_SPIRV}.unoptimized
+                COMMAND ${VULKAN_SPIRV_OPT_EXEC} ${OUTPUT_SPIRV}.unoptimized -Os -o ${OUTPUT_SPIRV}
+                COMMAND ${VULKAN_SPIRV_VAL_EXEC} ${OUTPUT_SPIRV}
+                MAIN_DEPENDENCY ${INPUT_GLSL}
+                COMMENT "Compiling GLSL shader ${INPUT_GLSL_REL} into SPIR-V (${OUTPUT_SPIRV_NAME})"
+                VERBATIM)
+        endif()
+
         list(APPEND OUTPUT_SPIRV_FILES ${OUTPUT_SPIRV})
     endforeach()
     set(${generated_files} "${OUTPUT_SPIRV_FILES}" PARENT_SCOPE)
--- src/renderer/shader/mesh_transformed_shaded.vert
+++ src/renderer/shader/mesh_transformed_shaded.vert
@@ -1,32 +1,39 @@
-#version 450 core
-
-layout(binding = 0) uniform UniformBufferObject
+struct UniformBufferObject
 {
-    mat4 model;
-    mat4 viewProj;
-    mat3 modelViewInvT;
+    float4x4 model;
+    float4x4 viewProj;
+    float3x3 modelViewInvT;
     float timeMs;
-} uniformBuffer;
-
-layout(location = 0) in vec3 in_PositionMS;
-layout(location = 1) in vec3 in_NormalMS;
-layout(location = 2) in vec2 in_UV;
-
-layout(location = 0) out vec3 out_NormalVS;
-layout(location = 1) out vec2 out_UV;
-
-out gl_PerVertex
-{
-    vec4 gl_Position;
 };
 
-void main()
-{
-    const vec3 positionMS = in_PositionMS * (1.0 + 0.2 * sin(uniformBuffer.timeMs * 0.2));
-    const vec4 positionWS = uniformBuffer.model * vec4(positionMS, 1.0);
-    const vec4 positionCS = uniformBuffer.viewProj * positionWS;
+UniformBufferObject uniformBuffer : register(b0);
 
-    gl_Position = positionCS;
-    out_NormalVS = uniformBuffer.modelViewInvT * in_NormalMS;
-    out_UV = in_UV;
+struct VS_INPUT
+{
+    float3 PositionMS;
+    float3 NormalMS;
+    float2 UV;
+};
+
+struct VS_OUTPUT
+{
+    float4 positionCS : SV_Position;
+    float3 NormalVS;
+    float2 UV;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+    const float3 positionMS = input.PositionMS * (1.0 + 0.2 * sin(uniformBuffer.timeMs * 0.2));
+    const float4 positionWS = uniformBuffer.model * float4(positionMS, 1.0);
+    const float4 positionCS = uniformBuffer.viewProj * positionWS;
+
+    VS_OUTPUT output;
+
+    output.positionCS = positionCS;
+
+    output.NormalVS = uniformBuffer.modelViewInvT * input.NormalMS;
+    output.UV = input.UV;
+
+    return output;
 }
