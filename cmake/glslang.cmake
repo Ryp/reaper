25c00366c57d71fb4828fa58c6c71455fd678494
Ryp <thibault.schueller@protonmail.com>
2020-01-18 01:38:43 +0300

    cmake: rename glslang variables for clarity.

--- cmake/glslang.cmake
+++ cmake/glslang.cmake
@@ -37,10 +37,10 @@ function(add_glslang_spirv_targets shader_root_folder use_hlsl generated_files)
         message(FATAL_ERROR "No shaders were passed to the function")
     endif()
     set(OUTPUT_SPIRV_FILES)
-    foreach(INPUT_GLSL IN LISTS ARGN)
+    foreach(INPUT_SHADER IN LISTS ARGN)
         # Build the correct output name and path
-        file(RELATIVE_PATH INPUT_GLSL_REL ${shader_root_folder} ${INPUT_GLSL})
-        set(OUTPUT_SPIRV "${CMAKE_BINARY_DIR}/spv/${INPUT_GLSL_REL}.spv")
+        file(RELATIVE_PATH INPUT_SHADER_REL ${shader_root_folder} ${INPUT_SHADER})
+        set(OUTPUT_SPIRV "${CMAKE_BINARY_DIR}/spv/${INPUT_SHADER_REL}.spv")
         get_filename_component(OUTPUT_SPIRV_PATH ${OUTPUT_SPIRV} DIRECTORY)
         get_filename_component(OUTPUT_SPIRV_NAME ${OUTPUT_SPIRV} NAME)
 
@@ -49,20 +49,20 @@ function(add_glslang_spirv_targets shader_root_folder use_hlsl generated_files)
         if(${use_hlsl})
             add_custom_command(OUTPUT ${OUTPUT_SPIRV}
                 COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_SPIRV_PATH}
-                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} --target-env spirv1.3 -g -e main -V -D ${INPUT_GLSL} -o ${OUTPUT_SPIRV}.unoptimized
+                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} --target-env spirv1.3 -g -e main -V -D ${INPUT_SHADER} -o ${OUTPUT_SPIRV}.unoptimized
                 COMMAND ${VULKAN_SPIRV_OPT_EXEC} ${OUTPUT_SPIRV}.unoptimized --legalize-hlsl -Os -o ${OUTPUT_SPIRV}
                 COMMAND ${VULKAN_SPIRV_VAL_EXEC} ${OUTPUT_SPIRV}
-                MAIN_DEPENDENCY ${INPUT_GLSL}
-                COMMENT "Compiling HLSL shader ${INPUT_GLSL_REL} into SPIR-V (${OUTPUT_SPIRV_NAME})"
+                MAIN_DEPENDENCY ${INPUT_SHADER}
+                COMMENT "Compiling HLSL shader ${INPUT_SHADER_REL} to SPIR-V (${OUTPUT_SPIRV_NAME})"
                 VERBATIM)
         else()
             add_custom_command(OUTPUT ${OUTPUT_SPIRV}
                 COMMAND ${CMAKE_COMMAND} -E make_directory ${OUTPUT_SPIRV_PATH}
-                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} --target-env spirv1.3 -g -V ${INPUT_GLSL} -o ${OUTPUT_SPIRV}.unoptimized
+                COMMAND ${VULKAN_GLSLANGVALIDATOR_EXEC} --target-env spirv1.3 -g -V ${INPUT_SHADER} -o ${OUTPUT_SPIRV}.unoptimized
                 COMMAND ${VULKAN_SPIRV_OPT_EXEC} ${OUTPUT_SPIRV}.unoptimized -Os -o ${OUTPUT_SPIRV}
                 COMMAND ${VULKAN_SPIRV_VAL_EXEC} ${OUTPUT_SPIRV}
-                MAIN_DEPENDENCY ${INPUT_GLSL}
-                COMMENT "Compiling GLSL shader ${INPUT_GLSL_REL} into SPIR-V (${OUTPUT_SPIRV_NAME})"
+                MAIN_DEPENDENCY ${INPUT_SHADER}
+                COMMENT "Compiling GLSL shader ${INPUT_SHADER_REL} to SPIR-V (${OUTPUT_SPIRV_NAME})"
                 VERBATIM)
         endif()
 
