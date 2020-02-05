3a09d561492450f0fcde0c49903b64f93986c915
Ryp <thibault.schueller@protonmail.com>
2020-02-06 01:13:39 +0300

    vulkan: support multiple instances during culling
    One dispatch is responsible for culling one instance and appending one
    command in the command buffer.
    One group inside of a dispatch is doing a cluster of 256 triangles.
    All indices are tightly packed in the index buffer created by culling.
    There's still an issue with disappearing triangles (a few on the total
    mesh).
    
    Add new per instance mesh animation to better see culling results in the
    capture.
    
    Add shared lib/ folder for shaders. The 'base.hlsl' header is now
    mandatory for every shaders.

--- src/renderer/shader/cull_triangle_batch.comp.hlsl
+++ src/renderer/shader/cull_triangle_batch.comp.hlsl
@@ -1,47 +1,64 @@
-[[vk::binding(0, 0)]]
-ByteAddressBuffer Indices;
-
-[[vk::binding(1, 0)]]
-ByteAddressBuffer VertexPositions;
+#include "lib/base.hlsl"
 
 struct CullInstanceParams
 {
     float4x4 ms_to_cs_matrix;
 };
 
-[[vk::binding(2, 0)]]
-StructuredBuffer<CullInstanceParams> instance_params;
-
-[[vk::binding(3, 0)]]
-RWByteAddressBuffer IndicesOut;
-
-[[vk::binding(4, 0)]]
-RWByteAddressBuffer DrawCommandOut;
-
-[[vk::binding(5, 0)]]
-RWByteAddressBuffer DrawCountOut;
-
-groupshared uint lds_total_triangle_count;
-
-static const uint3 thread_count = uint3(1024, 1, 1);
-
-[numthreads(thread_count.x, thread_count.y, thread_count.z)]
-void main(uint3 gtid : SV_GroupThreadID,
-          uint3 gid  : SV_GroupID,
-          uint3 dtid : SV_DispatchThreadID,
-          uint  gi   : SV_GroupIndex)
+struct PushConstants
 {
-    if (gi == 0)
-        lds_total_triangle_count = 0;
+    uint commandIndex;
+    uint triangleCount;
+    uint firstIndex;
+};
 
-    // FIXME can we remove it?
-    GroupMemoryBarrierWithGroupSync();
+static const uint ComputeCullingBatchSize = 256;
 
-    const uint input_triangle_index = dtid.x * 3;
+//------------------------------------------------------------------------------
+// Input
 
-    const uint index0 = Indices.Load((input_triangle_index + 0) * 4);
-    const uint index1 = Indices.Load((input_triangle_index + 1) * 4);
-    const uint index2 = Indices.Load((input_triangle_index + 2) * 4);
+VK_PUSH_CONSTANT() ConstantBuffer<PushConstants> consts;
+
+VK_BINDING(0, 0) ByteAddressBuffer Indices;
+VK_BINDING(1, 0) ByteAddressBuffer VertexPositions;
+VK_BINDING(2, 0) StructuredBuffer<CullInstanceParams> instance_params;
+
+//------------------------------------------------------------------------------
+// Output
+
+VK_BINDING(3, 0) RWByteAddressBuffer IndicesOut;
+VK_BINDING(4, 0) RWByteAddressBuffer DrawCommandOut;
+VK_BINDING(5, 0) globallycoherent RWByteAddressBuffer DrawCountOut;
+
+//------------------------------------------------------------------------------
+
+[numthreads(ComputeCullingBatchSize, 1, 1)]
+void main(/*uint3 gtid : SV_GroupThreadID,*/
+          /*uint3 gid  : SV_GroupID,*/
+          uint3 dtid : SV_DispatchThreadID,
+          /*uint  gi   : SV_GroupIndex*/)
+{
+    if (dtid.x == 0)
+    {
+        // FIXME Clear each frame
+        if (consts.commandIndex == 0)
+        {
+            DrawCountOut.Store(0, 0);
+            DrawCountOut.Store(4, 0);
+        }
+
+        DrawCountOut.Store(8, 0xFFFFFFFF);
+        DrawCountOut.Store(12, 0);
+    }
+
+    AllMemoryBarrierWithGroupSync(); // FIXME
+    //GroupMemoryBarrierWithGroupSync();
+
+    const uint input_triangle_index_offset = consts.firstIndex + dtid.x * 3;
+
+    const uint index0 = Indices.Load((input_triangle_index_offset + 0) * 4);
+    const uint index1 = Indices.Load((input_triangle_index_offset + 1) * 4);
+    const uint index2 = Indices.Load((input_triangle_index_offset + 2) * 4);
 
     const uint vertex_size_in_bytes = 3 * 4;
 
@@ -50,34 +67,39 @@ void main(uint3 gtid : SV_GroupThreadID,
     const float3 vpos1_ms = asfloat(VertexPositions.Load4(index1 * vertex_size_in_bytes).xyz);
     const float3 vpos2_ms = asfloat(VertexPositions.Load4(index2 * vertex_size_in_bytes).xyz);
 
-    // FIXME get correct instance matrix
-    const float4x4 ms_to_cs_matrix = instance_params[0].ms_to_cs_matrix;
+    const float4x4 ms_to_cs_matrix = instance_params[consts.commandIndex].ms_to_cs_matrix;
 
-    const float3 vpos0_cs = mul(float4(vpos0_ms, 1.0), ms_to_cs_matrix).xyz;
-    const float3 vpos1_cs = mul(float4(vpos1_ms, 1.0), ms_to_cs_matrix).xyz;
-    const float3 vpos2_cs = mul(float4(vpos2_ms, 1.0), ms_to_cs_matrix).xyz;
+    const float4 vpos0_cs = float4(vpos0_ms, 1.0) * ms_to_cs_matrix;
+    const float4 vpos1_cs = float4(vpos1_ms, 1.0) * ms_to_cs_matrix;
+    const float4 vpos2_cs = float4(vpos2_ms, 1.0) * ms_to_cs_matrix;
 
-    const float3 v0v1_cs = vpos1_cs - vpos0_cs;
-    const float3 v0v2_cs = vpos2_cs - vpos0_cs;
+    const float3 v0v1_cs = vpos1_cs.xyz - vpos0_cs.xyz;
+    const float3 v0v2_cs = vpos2_cs.xyz - vpos0_cs.xyz;
 
-    const bool is_visible = cross(v0v1_cs, v0v2_cs).z >= 0.f;
+    const bool is_ccw = cross(v0v1_cs, v0v2_cs).z <= 0.f;
+    const bool is_enabled = dtid.x < consts.triangleCount;
+
+    const bool is_visible = is_ccw && is_enabled;
+    //const bool is_visible = is_enabled;
 
     const uint visible_count = WaveActiveCountBits(is_visible);
     const uint visible_prefix_count = WavePrefixCountBits(is_visible);
 
-    const uint lane_cout = WaveGetLaneCount();
-
     // Synchronize waves
-    uint global_output_index;
+    uint wave_triangle_offset;
 
-    if (gi % lane_cout == 0)
+    if (WaveIsFirstLane())
     {
-        InterlockedAdd(lds_total_triangle_count, visible_count, global_output_index);
+        DrawCountOut.InterlockedAdd(4, visible_count, wave_triangle_offset);
+
+        DrawCountOut.InterlockedMin(8, wave_triangle_offset);
+
+        DrawCountOut.InterlockedAdd(12, visible_count);
     }
 
-    global_output_index = WaveReadLaneAt(global_output_index, 0);
+    wave_triangle_offset = WaveReadLaneAt(wave_triangle_offset, 0);
 
-    const uint output_triangle_index = global_output_index + visible_prefix_count - 1;
+    const uint output_triangle_index = wave_triangle_offset + visible_prefix_count - 1;
 
     if (is_visible)
     {
@@ -86,16 +108,21 @@ void main(uint3 gtid : SV_GroupThreadID,
         IndicesOut.Store((output_triangle_index * 3 + 2) * 4, index2);
     }
 
-    GroupMemoryBarrierWithGroupSync();
+    AllMemoryBarrierWithGroupSync();
 
-    if (gi == 0)
+    if (dtid.x == 0)
     {
-        const uint index_count_offset = 0;
-        DrawCommandOut.Store((index_count_offset + 0) * 4, lds_total_triangle_count * 3);
-        DrawCommandOut.Store((index_count_offset + 5) * 4, lds_total_triangle_count * 3);
-        DrawCommandOut.Store((index_count_offset + 10) * 4, lds_total_triangle_count * 3);
+        uint draw_command_index;
+        DrawCountOut.InterlockedAdd(0, uint(1), draw_command_index); // FIXME Cast is needed for glslang
 
-        const uint dummy_draw_count = 2; // FIXME set to 2 to demonstrate the feature
-        DrawCountOut.Store(0, dummy_draw_count);
+        uint min_triangle_offset = DrawCountOut.Load(8);
+        uint triangle_count = DrawCountOut.Load(12);
+
+        const uint draw_command_size = 5;
+        DrawCommandOut.Store((draw_command_index * draw_command_size + 0) * 4, triangle_count * 3);
+        DrawCommandOut.Store((draw_command_index * draw_command_size + 1) * 4, 1);
+        DrawCommandOut.Store((draw_command_index * draw_command_size + 2) * 4, min_triangle_offset * 3);
+        DrawCommandOut.Store((draw_command_index * draw_command_size + 3) * 4, 0);
+        DrawCommandOut.Store((draw_command_index * draw_command_size + 4) * 4, consts.commandIndex);
     }
 }
