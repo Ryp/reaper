6a9a3e11d7e2def5fff3d41abbed4ba7d19d61de
Ryp <thibault.schueller@protonmail.com>
2020-02-08 00:09:03 +0300

    hlsl: add more shared types to cover basics

--- src/renderer/shader/share/types.hlsl
+++ src/renderer/shader/share/types.hlsl
@@ -10,14 +10,30 @@
 
 #ifdef REAPER_SHADER_CODE
     #define sl_uint     uint
+    #define sl_uint2    uint2
+    #define sl_uint3    uint3
+    #define sl_uint4    uint4
+
     #define sl_float    float
+    #define sl_float2   float2
+    #define sl_float3   float3
+    #define sl_float4   float4
+
     #define sl_float4x4 float4x4
     #define sl_float3x3 float3x3
 #else
     using sl_uint       = u32;
+    using sl_uint2      = glm::uvec2;
+    using sl_uint3      = glm::uvec3;
+    using sl_uint4      = glm::uvec4;
+
     using sl_float      = float;
-    using sl_float4x4   = glm::mat4;
-    using sl_float3x3   = glm::mat3x4; // FIXME padding
+    using sl_float2     = glm::fvec2;
+    using sl_float3     = glm::fvec3;
+    using sl_float4     = glm::fvec4;
+
+    using sl_float4x4   = glm::fmat4;
+    using sl_float3x3   = glm::fmat3x4; // FIXME padding
 #endif
 
 #endif
