c35c40b5e09cb5a916b51fb391375119d56ff8d7
Ryp <thibault.schueller@protonmail.com>
2020-02-18 00:11:16 +0300

    vulkan: separate pipeline creation code into own file

---
+++ src/renderer/vulkan/Culling.cpp
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Reaper
+///
+/// Copyright (c) 2015-2019 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#include "Culling.h"
+
+#include "Shader.h"
+#include "SwapchainRendererBase.h"
+
+#include "common/Log.h"
+#include "common/ReaperRoot.h"
+
+#include <array>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "renderer/shader/share/culling.hlsl"
+
+namespace Reaper
+{
+CullPipelineInfo create_cull_pipeline(ReaperRoot& root, VulkanBackend& backend)
+{
+    std::array<VkDescriptorSetLayoutBinding, 6> descriptorSetLayoutBinding = {
+        VkDescriptorSetLayoutBinding{0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+        VkDescriptorSetLayoutBinding{1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+        VkDescriptorSetLayoutBinding{2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+        VkDescriptorSetLayoutBinding{3, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+        VkDescriptorSetLayoutBinding{4, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+        VkDescriptorSetLayoutBinding{5, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1, VK_SHADER_STAGE_COMPUTE_BIT, nullptr},
+    };
+
+    VkDescriptorSetLayoutCreateInfo descriptorSetLayoutInfo = {
+        VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO, nullptr, 0,
+        static_cast<u32>(descriptorSetLayoutBinding.size()), descriptorSetLayoutBinding.data()};
+
+    VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
+    Assert(vkCreateDescriptorSetLayout(backend.device, &descriptorSetLayoutInfo, nullptr, &descriptorSetLayout)
+           == VK_SUCCESS);
+
+    log_debug(root, "vulkan: created descriptor set layout with handle: {}", static_cast<void*>(descriptorSetLayout));
+
+    const VkPushConstantRange cullPushConstantRange = {VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(CullPushConstants)};
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+                                                     nullptr,
+                                                     VK_FLAGS_NONE,
+                                                     1,
+                                                     &descriptorSetLayout,
+                                                     1,
+                                                     &cullPushConstantRange};
+
+    VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
+    Assert(vkCreatePipelineLayout(backend.device, &pipelineLayoutInfo, nullptr, &pipelineLayout) == VK_SUCCESS);
+
+    log_debug(root, "vulkan: created pipeline layout with handle: {}", static_cast<void*>(pipelineLayout));
+
+    VkShaderModule        computeShader = VK_NULL_HANDLE;
+    const char*           fileName = "./build/spv/cull_triangle_batch.comp.spv";
+    const char*           entryPoint = "main";
+    VkSpecializationInfo* specialization = nullptr;
+
+    vulkan_create_shader_module(computeShader, backend.device, fileName);
+
+    VkPipelineShaderStageCreateInfo shaderStage = {VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+                                                   nullptr,
+                                                   0,
+                                                   VK_SHADER_STAGE_COMPUTE_BIT,
+                                                   computeShader,
+                                                   entryPoint,
+                                                   specialization};
+
+    VkComputePipelineCreateInfo pipelineCreateInfo = {VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+                                                      nullptr,
+                                                      0,
+                                                      shaderStage,
+                                                      pipelineLayout,
+                                                      VK_NULL_HANDLE, // do not care about pipeline derivatives
+                                                      0};
+
+    VkPipeline      pipeline = VK_NULL_HANDLE;
+    VkPipelineCache cache = VK_NULL_HANDLE;
+
+    Assert(vkCreateComputePipelines(backend.device, cache, 1, &pipelineCreateInfo, nullptr, &pipeline) == VK_SUCCESS);
+
+    vkDestroyShaderModule(backend.device, computeShader, nullptr);
+
+    log_debug(root, "vulkan: created compute pipeline with handle: {}", static_cast<void*>(pipeline));
+
+    return CullPipelineInfo{pipeline, pipelineLayout, descriptorSetLayout};
+}
+} // namespace Reaper
---
+++ src/renderer/vulkan/Culling.h
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Reaper
+///
+/// Copyright (c) 2015-2019 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "api/Vulkan.h"
+
+namespace Reaper
+{
+struct ReaperRoot;
+struct VulkanBackend;
+
+struct CullPipelineInfo
+{
+    VkPipeline            pipeline;
+    VkPipelineLayout      pipelineLayout;
+    VkDescriptorSetLayout descSetLayout;
+};
+
+CullPipelineInfo create_cull_pipeline(ReaperRoot& root, VulkanBackend& backend);
+} // namespace Reaper
