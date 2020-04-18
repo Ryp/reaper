6f5d9a7683c15c2fe03ea8e7a3c1c745b9c31ebe
Ryp <thibault.schueller@protonmail.com>
2020-04-18 20:29:33 +0300

    renderer: improve lighting a bit
    Added lambert diffuse support.

--- src/renderer/shader/mesh_transformed_shaded.frag.hlsl
+++ src/renderer/shader/mesh_transformed_shaded.frag.hlsl
@@ -7,9 +7,10 @@ VK_BINDING(0, 0) ConstantBuffer<DrawPassParams> pass_params;
 
 struct PS_INPUT
 {
-    float4 positionCS : SV_Position;
-    float3 NormalVS : TEXCOORD0;
-    float2 UV : TEXCOORD1;
+    float4 PositionCS : SV_Position;
+    float3 PositionVS : TEXCOORD0;
+    float3 NormalVS : TEXCOORD1;
+    float2 UV : TEXCOORD2;
 };
 
 struct PS_OUTPUT
@@ -19,17 +20,25 @@ struct PS_OUTPUT
 
 PS_OUTPUT main(PS_INPUT input)
 {
-    const float3 lightDirectionVS = float3(0.0, 1.0, 0.0);
+    const float3 object_to_light_vs = pass_params.light_position_vs - input.PositionVS;
+    const float3 light_direction_vs = normalize(object_to_light_vs);
+
+    const float light_distance_sq = dot(object_to_light_vs, object_to_light_vs);
+    const float light_distance_fade = rcp(1.0 + light_distance_sq);
+
     const float3 normalVS = normalize(input.NormalVS);
 
-    const float NdotL = saturate(dot(normalVS, pass_params.light_direction_vs));
+    const float NdotL = saturate(dot(normalVS, light_direction_vs));
 
-    const float3 uvChecker = float3(frac(input.UV * 10.0).xy, 1.0);
-    const float3 color = NdotL;
+    const float3 object_albedo = float3(0.9, 0.9, 0.9);
+    const float3 light_color = float3(0.8, 0.5, 0.2);
+    const float light_intensity = 8.f;
+
+    const float3 lambert_diffuse = object_albedo * light_color * light_intensity * NdotL * light_distance_fade;
 
     PS_OUTPUT output;
 
-    output.color = float4(color, 1.0);
+    output.color = float4(lambert_diffuse, 1.0);
 
     return output;
 }
