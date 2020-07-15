01edf90a591368ab3be1eb59be2563c00a178884
Ryp <ryp.sqrt@gmail.com>
2020-07-15 17:03:32 +0300

    vulkan: separate renderpass-related files

--- src/renderer/vulkan/renderpass/ShadowMap.h
+++ src/renderer/vulkan/renderpass/ShadowMap.h
@@ -7,19 +7,24 @@
 
 #pragma once
 
-#include "api/Vulkan.h"
+#include "renderer/vulkan/api/Vulkan.h"
 
 namespace Reaper
 {
 struct ReaperRoot;
 struct VulkanBackend;
+struct GPUTextureProperties;
 
-struct CullPipelineInfo
+struct ShadowMapPipelineInfo
 {
     VkPipeline            pipeline;
     VkPipelineLayout      pipelineLayout;
     VkDescriptorSetLayout descSetLayout;
 };
 
-CullPipelineInfo create_cull_pipeline(ReaperRoot& root, VulkanBackend& backend);
+VkRenderPass create_shadow_raster_pass(ReaperRoot& root, VulkanBackend& backend,
+                                       const GPUTextureProperties& shadowMapProperties);
+
+ShadowMapPipelineInfo create_shadow_map_pipeline(ReaperRoot& root, VulkanBackend& backend, VkRenderPass renderPass,
+                                                 u32 shadowMapRes);
 } // namespace Reaper
