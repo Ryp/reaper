587022eba413b5571871fd0d33fae600dac3e3ac
Ryp <thibault.schueller@protonmail.com>
2020-09-13 17:03:42 +0200

    vulkan: move more code out of TestGraphics

--- src/renderer/vulkan/renderpass/MainPass.h
+++ src/renderer/vulkan/renderpass/MainPass.h
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include "renderer/vulkan/Buffer.h"
 #include "renderer/vulkan/api/Vulkan.h"
 
 namespace Reaper
@@ -22,8 +23,17 @@ struct ReaperRoot;
 struct VulkanBackend;
 struct GPUTextureProperties;
 
-VkRenderPass create_main_raster_pass(ReaperRoot& /*root*/, VulkanBackend& backend,
-                                     const GPUTextureProperties& depthProperties);
+VkRenderPass create_main_pass(ReaperRoot& /*root*/, VulkanBackend& backend,
+                              const GPUTextureProperties& depthProperties);
 
-BlitPipelineInfo create_blit_pipeline(ReaperRoot& root, VulkanBackend& backend, VkRenderPass renderPass);
+BlitPipelineInfo create_main_pipeline(ReaperRoot& root, VulkanBackend& backend, VkRenderPass renderPass);
+
+struct MainPassResources
+{
+    BufferInfo drawPassConstantBuffer;
+    BufferInfo drawInstanceConstantBuffer;
+};
+
+MainPassResources create_main_pass_resources(ReaperRoot& root, VulkanBackend& backend);
+void              destroy_main_pass_resources(VulkanBackend& backend, MainPassResources& resources);
 } // namespace Reaper
