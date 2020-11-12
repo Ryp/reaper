362f04dd1b64c977d4548f6f995280458eb9bff4
Ryp <thibault.schueller@protonmail.com>
2020-11-13 00:15:18 +0100

    vulkan: allow bigger index buffer size

--- src/renderer/vulkan/renderpass/CullingConstants.h
+++ src/renderer/vulkan/renderpass/CullingConstants.h
@@ -13,6 +13,6 @@ constexpr u32 MaxIndirectDrawCount = 2000;
 constexpr u32 MaxCullPassCount = 4;
 constexpr u32 IndirectDrawCountCount = 2; // Second uint is for keeping track of total triangles
 constexpr u32 CullInstanceCountMax = 512;
-constexpr u32 DynamicIndexBufferSize = static_cast<u32>(2000_kiB);
+constexpr u32 DynamicIndexBufferSize = static_cast<u32>(4000_kiB);
 constexpr u32 IndexSizeBytes = 4;
 } // namespace Reaper
