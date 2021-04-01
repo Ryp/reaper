be2d207be6b17e464d0e627d0aede1c502a94fb8
Ryp <thibault.schueller@protonmail.com>
2021-04-01 13:30:12 +0300

    hdr: add color space output conversion
    Add DCI-P3 support
    Assume our scene uses REC709 for it's primaries

--- src/renderer/shader/lib/color_space.hlsl
+++ src/renderer/shader/lib/color_space.hlsl
@@ -8,6 +8,22 @@
 #ifndef LIB_COLOR_SPACE_INCLUDED
 #define LIB_COLOR_SPACE_INCLUDED
 
+// -----------------------------------------------------------------------------
+// Color Spaces
+// -----------------------------------------------------------------------------
+
+float3 rec709_to_srgb(float3 color_rec709)
+{
+    return color_rec709; // Primaries are the same
+}
+
+float3 srgb_to_rec709(float3 color_srgb)
+{
+    return color_srgb; // Primaries are the same
+}
+
+// -----------------------------------------------------------------------------
+
 float3 rec709_to_rec2020(float3 color_rec709)
 {
     static const float3x3 rec709_to_rec2020_matrix =
@@ -18,6 +34,44 @@ float3 rec709_to_rec2020(float3 color_rec709)
     };
 
     return mul(rec709_to_rec2020_matrix, color_rec709);
+}
+
+float3 rec2020_to_rec709(float3 color_rec2020)
+{
+    static const float3x3 rec2020_to_rec709_matrix =
+    {
+        1.660496, -0.587656, -0.072840,
+        -0.124547, 1.132895, -0.008348,
+        -0.018154, -0.100597, 1.118751
+    };
+
+    return mul(rec2020_to_rec709_matrix, color_rec2020);
+}
+
+// -----------------------------------------------------------------------------
+
+float3 rec709_to_dci_p3(float3 color_rec709)
+{
+    static const float3x3 rec709_to_dci_p3_matrix =
+    {
+        0.822458, 0.177542, 0.000000,
+        0.033193, 0.966807, 0.000000,
+        0.017085, 0.072410, 0.910505
+    };
+
+    return mul(rec709_to_dci_p3_matrix, color_rec709);
+}
+
+float3 dci_p3_to_rec709(float3 color_dci_p3)
+{
+    static const float3x3 dci_p3_to_rec709_matrix =
+    {
+        1.224947, -0.224947, 0.000000,
+        -0.042056, 1.042056, 0.000000,
+        -0.019641, -0.078651, 1.098291
+    };
+
+    return mul(dci_p3_to_rec709_matrix, color_dci_p3);
 }
 
 // -----------------------------------------------------------------------------
@@ -58,6 +112,18 @@ float3 srgb_eotf_inverse(float3 x)
 {
     // Looks like the gamma 2.2 inverse function
     return x < 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4);
+}
+
+// Replace pow() with cheaper sqrt()
+// Error < 0.4%
+float3 srgb_eotf_fast(float3 color)
+{
+    return color < 0.0031308 ? 12.92 * color : 1.13005 * sqrt(color - 0.00228) - 0.13448 * color + 0.005719;
+}
+
+float3 srgb_eotf_inverse_fast(float3 x)
+{
+    return x < 0.04045 ? x / 12.92 : -7.43605 * x - 31.24297 * sqrt(-0.53792 * x + 1.279924) + 35.34864;
 }
 
 // -----------------------------------------------------------------------------
