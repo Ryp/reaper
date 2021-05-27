99b3fedfa651c1052fce739fa57a192bdb8dbabd
Ryp <thibault.schueller@protonmail.com>
2021-05-28 00:26:48 +0300

    renderer: remove legacy shaders

--- src/renderer/shader/legacy/simple.frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 450 core
-
-out vec4 fragColor;
-
-void main(void)
-{
-    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
--- src/renderer/shader/legacy/simple.vert.glsl
+++
@@ -1,8 +0,0 @@
-#version 450 core
-
-in vec3 Position;
-
-void main(void)
-{
-    gl_Position = vec4(Position, 1.0);
-}
