09c41028c433f17dc5b06fc53c6a64c60960c410
Ryp <thibault.schueller@protonmail.com>
2022-05-10 23:40:43 +0300

    external: update fmt to latest

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -11,7 +11,7 @@ target_include_directories(doctest SYSTEM INTERFACE ${CMAKE_SOURCE_DIR}/external
 #///////////////////////////////////////////////////////////////////////////////
 
 add_library(fmt INTERFACE)
-target_include_directories(fmt SYSTEM INTERFACE ${CMAKE_SOURCE_DIR}/external/fmt)
+target_include_directories(fmt SYSTEM INTERFACE ${CMAKE_SOURCE_DIR}/external/fmt/include)
 target_compile_definitions(fmt INTERFACE FMT_HEADER_ONLY)
 
 #///////////////////////////////////////////////////////////////////////////////
