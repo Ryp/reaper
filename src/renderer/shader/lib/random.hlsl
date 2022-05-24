42b1e541bec30b7e93a1922adc45b0cb98bc3d0a
Ryp <thibault.schueller@protonmail.com>
2022-05-24 11:33:29 +0300

    shader: add Nvidia GPU hash

--- src/renderer/shader/lib/random.hlsl
+++ src/renderer/shader/lib/random.hlsl
@@ -8,9 +8,29 @@
 #ifndef LIB_RANDOM_INCLUDED
 #define LIB_RANDOM_INCLUDED
 
-float rand_neg_1_to_1(float2 n)
+// JCGT 2020 Hash Functions for GPU Rendering
+uint3 hash_pcg3d(uint3 v)
 {
-    return frac(sin(dot(n.xy, float2(12.9898, 78.233)))* 43758.5453);
+    v = v * 1664525u + 1013904223u;
+    v.x += v.y*v.z; v.y += v.z*v.x; v.z += v.x*v.y;
+    v ^= v >> 16u;
+    v.x += v.y*v.z; v.y += v.z*v.x; v.z += v.x*v.y;
+    return v;
+}
+
+uint4 hash_pcg4d(uint4 v)
+{
+    v = v * 1664525u + 1013904223u;
+    v.x += v.y*v.w; v.y += v.z*v.x; v.z += v.x*v.y; v.w += v.y*v.z;
+    v ^= v >> 16u;
+    v.x += v.y*v.w; v.y += v.z*v.x; v.z += v.x*v.y; v.w += v.y*v.z;
+    return v;
+}
+
+// https://stackoverflow.com/questions/12964279/whats-the-origin-of-this-glsl-rand-one-liner
+float hash_canonical(float2 n)
+{
+    return frac(sin(dot(n.xy, float2(12.9898, 78.233))) * 43758.5453);
 }
 
 #endif
