1a992197dd4b237a12c16dac8a2bce8b31a31d02
Ryp <thibault.schueller@protonmail.com>
2022-05-30 20:15:29 +0300

    cmake: use glslangValidator from FindVulkan

--- cmake/glslang.cmake
+++ cmake/glslang.cmake
@@ -5,9 +5,9 @@
 #// This file is distributed under the MIT License
 #///////////////////////////////////////////////////////////////////////////////
 
-find_program(VULKAN_GLSLANGVALIDATOR_EXEC glslangValidator HINTS
-    "$ENV{VULKAN_SDK}/Bin"
-    "$ENV{VK_SDK_PATH}/Bin")
+find_package(Vulkan REQUIRED)
+
+get_property(GLSLANGVALIDATOR_EXEC TARGET Vulkan::glslangValidator PROPERTY LOCATION)
 
 find_program(VULKAN_SPIRV_OPT_EXEC spirv-opt HINTS
     "$ENV{VULKAN_SDK}/Bin"
@@ -21,11 +21,11 @@ find_program(VULKAN_SPIRV_VAL_EXEC spirv-val HINTS
     "$ENV{VULKAN_SDK}/Bin"
     "$ENV{VK_SDK_PATH}/Bin")
 
-if(NOT VULKAN_GLSLANGVALIDATOR_EXEC)
-    message(FATAL_ERROR "${VULKAN_GLSLANGVALIDATOR_EXEC} not found")
+if(NOT GLSLANGVALIDATOR_EXEC)
+    message(FATAL_ERROR "${GLSLANGVALIDATOR_EXEC} not found")
 else()
     # Get the output from --version
-    set(GLSLANGVALIDATOR_VERSION_CMD ${VULKAN_GLSLANGVALIDATOR_EXEC} --version)
+    set(GLSLANGVALIDATOR_VERSION_CMD ${GLSLANGVALIDATOR_EXEC} --version)
     execute_process(COMMAND ${GLSLANGVALIDATOR_VERSION_CMD} OUTPUT_VARIABLE GLSLANGVALIDATOR_VERSION_FULL_STRING)
 
     # Parse it and extract major, minor and patch
