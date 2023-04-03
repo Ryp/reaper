397587481f1356e69f57363e176b1515d1cb44eb
Ryp <thibault.schueller@protonmail.com>
2023-04-03 23:32:03 +0800

    fix: hlsl interop matrix test
    Identity matrices weren't correctly initialized

--- src/renderer/test/hlsl/float_matrix.cpp
+++ src/renderer/test/hlsl/float_matrix.cpp
@@ -19,7 +19,7 @@ TEST_CASE("HLSL float matrix types")
         static_assert(alignof(hlsl_matrix3x3<ColumnMajor, f32>) == 4 * sizeof(float));
         static_assert(sizeof(hlsl_matrix3x3<ColumnMajor, f32>) == 12 * sizeof(float));
 
-        hlsl_float3x3 identity = glm::fmat3();
+        hlsl_float3x3 identity = glm::fmat3(1.f);
 
         CHECK_EQ(identity.element[0].x, 1.f);
         CHECK_EQ(identity.element[1].y, 1.f);
@@ -40,7 +40,7 @@ TEST_CASE("HLSL float matrix types")
         static_assert(alignof(hlsl_matrix3x4<ColumnMajor, f32>) == 4 * sizeof(float));
         static_assert(sizeof(hlsl_matrix3x4<ColumnMajor, f32>) == 16 * sizeof(float));
 
-        hlsl_float3x4 identity = glm::fmat4x3();
+        hlsl_float3x4 identity = glm::fmat4x3(1.f);
 
         CHECK_EQ(identity.element[0].x, 1.f);
         CHECK_EQ(identity.element[1].y, 1.f);
@@ -63,7 +63,7 @@ TEST_CASE("HLSL float matrix types")
         static_assert(alignof(hlsl_matrix4x4<ColumnMajor, f32>) == 4 * sizeof(float));
         static_assert(sizeof(hlsl_matrix4x4<ColumnMajor, f32>) == 16 * sizeof(float));
 
-        hlsl_float4x4 identity = glm::fmat4();
+        hlsl_float4x4 identity = glm::fmat4(1.f);
 
         CHECK_EQ(identity.element[0].x, 1.f);
         CHECK_EQ(identity.element[1].y, 1.f);
