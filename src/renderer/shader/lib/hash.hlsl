4b5974da615e7a622ec270519745bea7caddde36
Ryp <thibault.schueller@protonmail.com>
2023-08-04 08:21:07 +0200

    shader: add hash.hlsl

---
+++ src/renderer/shader/lib/hash.hlsl
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Reaper
+///
+/// Copyright (c) 2015-2023 Thibault Schueller
+/// This file is distributed under the MIT License
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef LIB_HASH_INCLUDED
+#define LIB_HASH_INCLUDED
+
+// https://www.reedbeta.com/blog/hash-functions-for-gpu-rendering/
+uint pcg_hash(uint input)
+{
+    uint state = input * 747796405u + 2891336453u;
+    uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+    return (word >> 22u) ^ word;
+}
+
+#endif
