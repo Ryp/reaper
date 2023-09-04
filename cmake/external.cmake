42149c096b45ba80360e2d5b650f1552845a2013
Ryp <thibault.schueller@protonmail.com>
2023-09-04 16:22:57 +0200

    external: add cgltf v1.13

--- cmake/external.cmake
+++ cmake/external.cmake
@@ -5,6 +5,11 @@
 #// This file is distributed under the MIT License
 #///////////////////////////////////////////////////////////////////////////////
 
+add_library(cgltf INTERFACE)
+target_include_directories(cgltf SYSTEM INTERFACE ${CMAKE_SOURCE_DIR}/external/cgltf)
+
+#///////////////////////////////////////////////////////////////////////////////
+
 add_library(doctest INTERFACE)
 target_include_directories(doctest SYSTEM INTERFACE ${CMAKE_SOURCE_DIR}/external/doctest)
 
