e970fae5b3b818569b5f0e2382017ffdc079fd58
Ryp <thibault.schueller@protonmail.com>
2023-10-07 12:16:39 +0200

    cmake: build bullet from source

--- cmake/bullet.cmake
+++ cmake/bullet.cmake
@@ -5,8 +5,27 @@
 #// This file is distributed under the MIT License
 #///////////////////////////////////////////////////////////////////////////////
 
-# Find actual dependency
-find_package(Bullet REQUIRED)
+set(BULLET_PATH ${CMAKE_SOURCE_DIR}/external/bullet3)
+
+option(USE_GRAPHICAL_BENCHMARK OFF)
+option(USE_SOFT_BODY_MULTI_BODY_DYNAMICS_WORLD OFF)
+option(ENABLE_VHACD OFF)
+option(BUILD_CPU_DEMOS OFF)
+option(USE_GLUT OFF)
+option(BUILD_ENET OFF)
+option(BUILD_CLSOCKET OFF)
+option(BUILD_EGL OFF)
+option(BUILD_OPENGL3_DEMOS OFF)
+option(BUILD_BULLET2_DEMOS OFF)
+option(BUILD_EXTRAS OFF)
+option(INSTALL_LIBS OFF)
+option(BUILD_UNIT_TESTS OFF)
+option(INSTALL_CMAKE_FILES OFF)
+
+add_subdirectory(${BULLET_PATH} ${CMAKE_BINARY_DIR}/external/bullet)
+
+set(BULLET_INCLUDE_DIRS ${BULLET_PATH}/src)
+set(BULLET_LIBRARIES LinearMath Bullet3Common BulletInverseDynamics BulletCollision BulletDynamics BulletSoftBody)
 
 # Wrap bullet for easier consumption
 add_library(bullet INTERFACE)
